434f750f5d596bc85eab43460c448373aff251be
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-28 06:08:03 +0800

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ConsoleApp4
+
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            int[] x = { 20, 10, 30, 50, 40 };
+            Array.Sort(x);
+
+            foreach(int i in x)
+            {
+                Console.Write(i);
+            }
+            Console.ReadLine();
+
+
+            Console.WriteLine("after sort elenments in decending order");
+
+        }
+
+    }
+}
+
